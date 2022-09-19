cbea9d62b21ac39879b21d6c301c3dae4aefba7c
Venti <1770109333@qq.com>
2022-09-19 09:21:41 +0800

    修改了代码

--- src/imSynthesis.cpp
+++ src/imSynthesis.cpp
@@ -14,28 +14,43 @@ void imSynthesis::integrate()
     vector<double> x_B, y_B;
     double quadrilateralA, quadrilateralB;
     bool identified;
+    double iou;
 
-	ifstream jfile("../example/result_2.json");
+	ifstream jfile("../json_example/result_2.json");
 	json data = json::parse(jfile);
     jfile.close();
 
-    string mode = "row";
+    string modelist[2] = {"row", "column"};
 
-    for(i=0; i<data[mode].size(); ++i){
-        for(k=0;k<4;k++){
-            x_A.push_back(data[mode][i]["location"]["points"][k]["x"]);
-            y_A.push_back(data[mode][i]["location"]["points"][k]["y"]);
-        }
-        identified  = false;
-        for(j=0; j<data["normal"].size(); ++j){
+    for(int l=0; l<2; ++l){
+        string mode = modelist[l];
+
+        for(i=0; i<data[mode].size(); ++i){
             for(k=0;k<4;k++){
-                x_B.push_back(data["normal"][i]["location"]["points"][k]["x"]);
-                y_B.push_back(data["normal"][i]["location"]["points"][k]["y"]);
+                x_A.push_back(data[mode][i]["location"]["points"][k]["x"]);
+                y_A.push_back(data[mode][i]["location"]["points"][k]["y"]);
+            }
+        identified  = false;
+            for(j=0; j<data["normal"].size(); ++j){
+                for(k=0;k<4;k++){
+                    x_B.push_back(data["normal"][i]["location"]["points"][k]["x"]);
+                    y_B.push_back(data["normal"][i]["location"]["points"][k]["y"]);
+                }
+                iou = IOU(x_A, x_B, y_A, y_B);
+
+                if(iou > _IoUThreshold){
+                    identified = true;
+                    break;
+                }
+            }
+            if(identified == false){
+                data["normal"].push_back(data[mode][i]);
             }
         }
+
     }
 
-    cout << data[mode][1]["location"]["points"][1]["x"] << endl;
+    //cout << data[mode][1]["location"]["points"][1]["x"] << endl;
 
 }
 
@@ -60,5 +75,7 @@ double IOU(vector<double> x_A, vector<double> x_B, vector<double> y_A, vector<do
 {
     vector<double> x_sum;
     vector<double> y_sum;
+
+    return 0.0;
 
 }
